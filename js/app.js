6b4666abc0b64c23cb4bad1ef8bef2e66b070d9b
Joe O'Connor <joconnorje@gmail.com>
2015-10-07 21:20:18 -0400

    Fixes to map and locations list; move locations data to Model

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 // Model
 
+//function Model() {
+//(note from Mark: lists of markers, places and other data)
+//}
+// var MODEL = new modle();
+//function ViewModel() {
+// define observables here; define and use Google Map objects; create other functions to communicate with
+// the model, observables, and Google Map objects
+//  }
+
 //var map;
 function initMap() {
 	var westford = {lat: 42.579167, lng: -71.438333};
   var mapOptions = {
     center: westford,
-    zoom: 17,
+    zoom: 17
     //disableDefaultUI: true
   }
   var map = new google.maps.Map(document.getElementById('map-container'),mapOptions);
@@ -20,7 +29,7 @@ var infowindow = new google.maps.InfoWindow({
   content: contentString
 });
 
-var marker = new google.maps.Marker({
+var marker = new google.maps.Marker({     //markers should b in the Model
     position: westford,
     map: map,
     title: 'Westford Center'
@@ -49,13 +58,18 @@ var marker = new google.maps.Marker({
 function LocationsViewModel() {
  var self = this;
 
- //Hard-coded locatsions
+ //Hard-coded locatsions - these should be in Model as they are data? yes, but then, in ViewModel, need to use
+ // something like this.getAllLocations = ko.computed(function() {
+//                    return model.Locations();
+//                });
+ }
 var locations = [
   {
    name: "Westford Common",
-   //marker:
-   //content
-   //lat and long
+   //marker: null
+   //  showItem: function(event, marker){}
+   //content: API
+   //latlong:
   },
   {
     name: "Parish Center for the Arts",
@@ -65,7 +79,7 @@ var locations = [
   },
   {
     name: "Fletcher Library",
-    //marker:
+    //marker: activateMarker();
     //content:
     //lat and long: 42.587874, -71.434396
   },
@@ -82,7 +96,7 @@ var locations = [
 ];
  self.location = ko.observableArray(locations);
 
-  //infooWindow: function(item) {
+  //infooWindow: function(item) {                   infoWindow object should be in Model
   //  display.infoWindow(item.name.toLowerCase());
   //},
 
@@ -90,12 +104,16 @@ var locations = [
 
   //search:
 
-}
+//}
 
   ko.applyBindings(new LocationsViewModel());
+
+//function activateMarker() {
+//  console.log("marker");
+//}
 //);
 
-
+// function searchLocations ()  get text from input and use to filter locations list
 
 //Class to represent a place in the Places list
 //function  currentPlace(name) {
