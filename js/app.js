f9788df2533efbec16858fc6ae7a65c82b4da3d9
Joe O'Connor <joconnorje@gmail.com>
2015-12-26 17:01:37 -0500

    Refactored to use a Location constructor and forEach (rather than for loops)

--- js/app.js
+++ js/app.js
@@ -2,42 +2,49 @@
 
  var locations = [
   {
-   name: "Tsongas Center",
-   marker: " ",
+   name: 'Tsongas Center',
+   marker: ' ',
    wikiURL: 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=Tsongas%20Center&callback=wikiCallback',
    content: ' ',
    latlng: {lat: 42.639444, lng: -71.314722}
   },
   {
-    name: "Pawtucket Canal",
-    marker: " ",
+    name: 'Pawtucket Canal',
+    marker: ' ',
     wikiURL: 'http://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=Pawtucket%20Canal&callback=wikiCallback',
     content: ' ',
     latlng: {lat: 42.644167, lng: -71.305833}
   },
   {
-    name: "UMass Lowell",
-    marker: " ",
+    name: 'UMass Lowell',
+    marker: ' ',
     wikiURL: 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=UMass%20Lowell&callback=wikiCallback',
     content: ' ',
     latlng: {lat: 42.642716, lng: -71.33453}
   },
   {
-    name: "Lowell Memorial Auditorium",
-    marker: " ",
+    name: 'Lowell Memorial Auditorium',
+    marker: ' ',
     wikiURL: 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=Lowell%20Memorial%20Auditorium&callback=wikiCallback',
     content: ' ',
     latlng: {lat: 42.645068, lng: -71.304172 }
   },
   {
-    name: "Westford Knight",
-    marker: " ",
+    name: 'Westford Knight',
+    marker: ' '  ,
     wikiURL: 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=Westford%20Knight&callback=wikiCallback',
     content: ' ',
     latlng: {lat: 42.587874, lng: -71.434396}
   }
 ];
 
+var Location = function(data) {
+  this.name = data.name;
+  this.marker = data.marker;
+  this.wikiURL = data.wikiURL;
+  this.content = ko.observable(data.content);
+  this.latlng = data.latlng;
+}
 
 //var markersArray = [];
 
@@ -48,15 +55,15 @@
 
 //Create map of Lowell using Google Maps API
 
-function initMap() {
+//function initMap() {
 
-	var lowell = {lat: 42.639444, lng: -71.314722};
-  var mapOptions = {
-    center: lowell,
-    zoom: 14,
-    disableDefaultUI: true
-  }
-  var map = new google.maps.Map(document.getElementById('map-container'),mapOptions);
+	//var lowell = {lat: 42.639444, lng: -71.314722};
+  //var mapOptions = {
+    //center: lowell,
+    //zoom: 14,
+    //disableDefaultUI: true
+  //}
+  //var map = new google.maps.Map(document.getElementById('map-container'),mapOptions);
 
 
 
@@ -128,45 +135,49 @@ function initMap() {
 //Get content for current location from data model
 // var content = locations[i].content;
 
+//function getWikiData(data) {
+  //var contentString = ('<div>' +  '<p>' + data[0] + '</p>' + '</div>');
+  //this.content = contentString;
+  //console.log(locations[i].content);
+  //console.log(data);
+//};
 
-for (var i = 0; i < locations.length; i++) {
+//for (var i = 0; i < locations.length; i++) {
 
-  var wikiCallback = function(data) {
-    console.dir(data[i]);
-  }
+  //var wikiCallback = function(data) {
+  //  console.dir(data[i]);
+  //}
 
 
-$(function() {
-  $.ajax({
-      url: locations[i].wikiURL,
-      dataType: 'jsonp',
-      success: function(data) {
-        //updateContent(data);
-        //console.log(data);
-        var contentString = ('<div>' +  '<p>' + data + '</p>' + '</div>');
-        locations[i].content = contentString;
-        console.log(locations[i].content);
+//$(function() {
+  //$.ajax({
+    //  url: locations[i].wikiURL,
+    //  dataType: 'jsonp',
+    //  success: getWikiData
+    //});
+//});
 
-      }
-  });
+  //var contentString = ('<div>' +  '<p>' + data + '</p>' + '</div>');
+  //locations[i].content = contentString;
+  //console.log(locations[i].content);
 
-});
+//});
 
 //contentString = '<div>' +  '<p>' + data + '</p>' + '</div>';
-        //locations[i].content = data;
-        console.log(locations[i].content);
+        //locations[i].content = contentString;
+        //console.log(locations[i].content);
 
-}
+//}
 
 
 //var currentLocation = locations[2]; // test; don't need these 2 lines
 //console.log(currentLocation);
 
 //Define initial infowindow and set content for currentLocation
-  var infowindow = new google.maps.InfoWindow(
-  {
-   content: 'initial content'
-  });
+  //var infowindow = new google.maps.InfoWindow(
+  //{
+   //content: 'initial content'
+  //});
   //};
 
 //locations.forEach
@@ -174,14 +185,14 @@ $(function() {
 // For each location, create a marker
 //var markersArray = [];
 
-for (var i = 0; i < locations.length; i++) {
+//for (var i = 0; i < locations.length; i++) {
   //create a marker on the map
-    var marker = new google.maps.Marker({
-    position: locations[i].latlng,
-    map: map,
-    content: locations[i].content,
-    animation: google.maps.Animation.DROP,
-  });
+  //  var marker = new google.maps.Marker({
+  //  position: locations[i].latlng,
+  //  map: map,
+  //  content: locations[i].content,
+  //  animation: google.maps.Animation.DROP,
+  //});
 
 //});
  //create an infowindow to open when marker is clicked
@@ -190,7 +201,7 @@ for (var i = 0; i < locations.length; i++) {
   //content: 'initial infowindow content'
  //});
 
-  locations[i].marker = marker; //Add marker to locations data in Model
+  //locations[i].marker = marker; //Add marker to locations data in Model
 
 //Get content for current location from data model
  // var content = locations[i].content;
@@ -234,33 +245,173 @@ for (var i = 0; i < locations.length; i++) {
 //});
 
 //Add listener for marker and open infowindow with current location content
-  marker.addListener('click', (function(markerRef, contentString) {  //on click, open infoWindow
-    return function() {
-    infowindow.setContent(contentString);
-    infowindow.open(map, markerRef);
-  }
-  })(marker,locations.content));
+//locations.forEach(function(locationItem) {
+  //marker.addListener('click', (function(markerRef, contentString) {  //on click, open infoWindow
+   // return function() {
+    //infowindow.setContent(contentString);
+    //infowindow.open(map, markerRef);
+  //}
+  //})(marker,locations.content));
+//});
   //var currentMarker = markersArray[2];
-}; //end for loop
+//}; //end for loop
 
-}; //end of initMap
+//}; //end of initMap need to return map?
 
 //var currentMarker = markersArray[2];
 //console.log(marker);
 //console.log(locations[1].content);
 
+//Create map of Lowell using Google Maps API
+
+function initMap() {
+
+  var lowell = {lat: 42.639444, lng: -71.314722};
+  var mapOptions = {
+    center: lowell,
+    zoom: 14,
+    disableDefaultUI: true
+  }
+  map = new google.maps.Map(document.getElementById('map-container'),mapOptions);
+
+ // function getWikiData(data) {
+ // var contentString = ('<div>' +  '<p>' + data[0] + '</p>' + '</div>');
+ // this.content = contentString;
+ // console.log(this.content);
+ // console.log(data);
+//};
+
+//for (var i = 0; i < locations.length; i++) {
+
+
+//$(function() {
+  //$.ajax({
+    //  url: locations[i].wikiURL,
+    //  dataType: 'jsonp',
+    //  success: getWikiData
+    //});
+//});
+
+  //var contentString = ('<div>' +  '<p>' + data + '</p>' + '</div>');
+  //locations[i].content = this.content;
+    //console.log(this.content);
+//} //end for loop
+
+//Define initial infowindow and set content for currentLocation
+  //infowindow = new google.maps.InfoWindow(
+  //{
+   //content: 'initial content'
+  //});
+
+  // Run viewModel here so that map is available to the viewModel
+ko.applyBindings(new viewModel() );
+
+}; //end of initMap
+
 function viewModel() {
   var self = this;
 
+
   //self.locationsList = ko.observableArray(locations.slice(0));
-  self.locationsList = ko.observableArray(locations);
-  //self.filteredList = ko.observableArray();
+  self.locationsList = ko.observableArray([]);
+  self.filteredList = ko.observableArray();
   //self.markersList = ko.observableArray(locations.marker);
   //self.markersArray = ko.observableArray(markersArray);
 
+  //Define initial infowindow and set content for currentLocation
+  infowindow = new google.maps.InfoWindow(
+  {
+   content: 'initial content'
+  });
 
-  self.setMarker = function(locations) {
+  //get data from wikipedia API and format content for infowindow display
+  function getWikiData(data) {
+    var contentString = ('<div>' +  '<p>' + data[0] + '</p>' + '</div>');
+    this.content = contentString;
+    infowindow.content = this.content;
+    //infowindow.setContent(this.content);
+
+    //console.log(this.content);
+    //console.log(data);
+  };
+
+function getContent(locationItem) {
+//for (var i = 0; i < locations.length; i++) {
+
+  //$(function() {
+    $.ajax({
+      url: locationItem.wikiURL,
+      dataType: 'jsonp',
+      success: getWikiData
+    });
+  //});
+}
+
+  //var contentString = ('<div>' +  '<p>' + data + '</p>' + '</div>');
+  //locations[i].content = this.content;
+  //console.log(locations[i].content);
+
+//});
+
+//contentString = '<div>' +  '<p>' + data + '</p>' + '</div>';
+        //locations[i].content = contentString;
+        //console.log(locations[0].content);
+
+//} //end for loop
+
+  // Create the locations using the Location constructor and add to the locationsList array
+  locations.forEach(function(locationItem) {
+    self.locationsList.push(new Location(locationItem) );
+  });
+
+  //create a marker on the map for each location
+  self.locationsList().forEach(function(locationItem) {
+  //locations.forEach(function(locationItem) {
+    var markerOptions = {
+      map: map,
+      position: locationItem.latlng,
+      animation: google.maps.Animation.DROP,
+    };
+    //var marker = new google.maps.Marker({
+      //position: self.latlng,
+      //map: self.map,
+    //content: locations[i].content,
+    //marker: locations[i].marker,
+      //animation: google.maps.Animation.DROP,
+    //});
+  locationItem.marker = new google.maps.Marker(markerOptions);
+  //self.marker = marker;
+  }); //end forEach
+
+  //Create wiki content for infowindow for each location
+  self.locationsList().forEach(function(locationItem) {
+    locationItem.content = getContent(locationItem);
+    console.log(locationItem.content);
+  });
+
+
+
+//console.log(locationItem.marker);
+
+//Add listener for marker and open infowindow with current location content
+self.locationsList().forEach(function(locationItem) {
+  locationItem.marker.addListener('click', (function(markerRef, contentString) {  //on click, open infoWindow
+    return function() {
+    //var contentString = this.content;
+    //infowindow.setContent(contentString);
+    locationItem.marker.setAnimation(google.maps.Animation.DROP); //activate associated marker
+    infowindow.open(map, markerRef);
+  }
+  })(locationItem.marker,locationItem.content));
+}); //end forEach
+
+console.log(self.locationsList()[0]);
+
+  //When a location in the list is clicked, activate the marker and open the infowindow
+  self.setMarker = function(location) {
+    var locations = location;
     console.log('click works');
+    //console.log(Location.marker);
     //map.setCenter(new google.maps.LatLng(locations.latlng ) );
     //map.setCenter(marker.getPosition());
     locations.marker.setAnimation(google.maps.Animation.DROP); //activate associated marker
@@ -268,7 +419,7 @@ function viewModel() {
     google.maps.event.trigger(locations.marker, 'click'); //open infowindow associated with marker
     //var infowindow = new google.maps.InfoWindow({
     //});
-    //infowindow.open(map,marker);
+    //infowindow.open(map,locations.marker);
   }
   //this.locationsList = ko.observableArray(locations.slice(1) );  //would need to get marker into locations list from markersArray
 
@@ -291,13 +442,15 @@ function viewModel() {
     //});
   //});
 
+  //Create a copy of the locations list to use to filter the list in response to search input
   self.filteredList = ko.observableArray();
 
-  self.locationsList().forEach(function (locations) {
+  locations.forEach(function (locations) {
       self.filteredList.push(locations);
   });
 
-// Use ko binding to user input in Serach box (self.query) to filter the display of locations and markers.
+
+// Use ko binding to user input in Search box (self.query) to filter the display of locations and markers.
 // http://codepen.io/prather-mcs/pen/KpjbNN
 
   self.filteringFunction = function(locations)  {
@@ -369,7 +522,7 @@ function viewModel() {
 
 }; //end of ViewModel
 // Run it
-ko.applyBindings(new viewModel() );
+//ko.applyBindings(new viewModel() );
 
 
 
